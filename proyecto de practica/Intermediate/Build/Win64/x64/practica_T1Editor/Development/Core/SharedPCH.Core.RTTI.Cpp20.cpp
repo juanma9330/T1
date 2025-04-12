5ad7e525742a75b5e2aada89e5f6d2d396ebfdd0
juanma9330 <123135043+juanma9330@users.noreply.github.com>
2025-04-12 16:32:06 -0300

    Delete proyecto de practica directory
    elimino repo viejo de lo creado para clase

--- proyecto de practica/Intermediate/Build/Win64/x64/practica_T1Editor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.38.33143
-#include "D:/unreal carpeta de proyectos/t1/practica/practica_T1/Intermediate/Build/Win64/x64/practica_T1Editor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
